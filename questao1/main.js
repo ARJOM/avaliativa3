5b73a3f9f43d772d25d7578630f7742819e56fd0
Ricart <antonio.ricart@academico.ifpb.edu.br>
2019-08-08 21:28:36 -0300

    criação de funções de exibição

--- questao1/main.js
+++ questao1/main.js
@@ -13,15 +13,30 @@ function cadastro() {
         var foto = document.getElementById("foto").value;
         var email = document.getElementById("email").value;
         var repo = document.getElementById("repo").value;
+        var latitude = document.getElementById("latitude").value;
+        var longitude = document.getElementById("longitude").value;
+        var localizacao = [latitude, longitude];
+        var linguagens = document.getElementById("linguagens").value;
+        var linguas = linguagens.split(", ");
 
-        var linguagens = document.getElementById("linguagens").value.split(", ");
-
-        var usuario = new Usuario(nome, foto, email, repo, null, linguagens);
-
+        var usuario = new Usuario(nome, foto, email, repo, localizacao, linguas);
 
         cadastrados = getObjectLocalStorage("cadastrados");
-        cadastrados.push(usuario);
-        setObjectLocalStorage("cadastrados", cadastrados);
+        if (validaInsert(email, cadastrados)){
+            cadastrados.push(usuario);
+            setObjectLocalStorage("cadastrados", cadastrados);
+        }
+        else{
+            window.alert("email já cadastrado");
+        }
+
+        document.getElementById("nome").value = "";
+        document.getElementById("foto").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("repo").value = "";
+        document.getElementById("latitude").value = "";
+        document.getElementById("longitude").value = "";
+        document.getElementById("linguagens").value = "";
 
     } else {
         window.alert("API Web Storage não encontrada");
@@ -30,10 +45,25 @@ function cadastro() {
 
 function exibe(){
     if (typeof(Storage) !== "undefined"){
+        cadastrados = getObjectLocalStorage("cadastrados");
+        var paragrafo = document.getElementById("exibe");
+        var resultado = "";
+        for (var i=0; i<cadastrados.length; i++){
+            var usuario = cadastrados[i];
+            var link = document.createElement("a");
+            resultado += "<div>"
+            resultado += "<p>"+usuario.nome+"</p>"
+            resultado += "</div"
+        }
+        paragrafo.innerHTML = resultado;
 
     } else {
         window.alert("API Web Storage não encontrada")
     }
+}
+
+function detalha(email){
+    window.alert()
 }
 
 //Classes
@@ -59,4 +89,14 @@ function setObjectLocalStorage(key,value){
 function getObjectLocalStorage(key){
 	var value = localStorage.getItem(key);
     return value && JSON.parse(value);
+}
+
+function validaInsert(email, cadastrados){
+    for (var i = 0; i<cadastrados.length; i++){
+        var usuario = cadastrados[i];
+        if (usuario.email == email){
+            return false;
+        }
+    }
+    return true;
 }
