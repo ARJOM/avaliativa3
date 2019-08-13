cd9b6ebd2fce340cb7bf92332e2191fbd620df0c
Ricart <antonio.ricart@academico.ifpb.edu.br>
2019-08-13 11:33:30 -0300

    adicionando links

--- questao2/main.js
+++ questao2/main.js
@@ -17,8 +17,10 @@ function carregaXMLRemotoNoticias(){
       descricao = $(this).find('summary').text();
       datapub = $(this).find('published').text();
       img = $(this).find('img')[0].getAttribute("src");
+      link = $(this).find('link')[0].getAttribute("href");
+      externo = "<a href=\""+link+"\">Veja mais.</a>";
       imagem = "<img src="+img+" />";
-      dadosLista += "<div> <h3>"+titulo+"</h3><h6>Publicado em:"+datapub+"</h6><p>"+descricao+"</p>"+imagem+"</div>";
+      dadosLista += "<div> <h3>"+titulo+"</h3><h6>Publicado em:"+datapub+"</h6><p>"+descricao+""+externo+"</p>"+imagem+"</div>";
     });
     document.getElementById("noticias").innerHTML = dadosLista;
   });
