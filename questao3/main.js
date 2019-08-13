78daf17c4d9c28a0e9c773f95383b1e7357e5af5
Ricart <antonio.ricart@academico.ifpb.edu.br>
2019-08-12 23:00:17 -0300

    finalizando o processo de exibição das informações do arquivo csv

--- questao3/main.js
+++ questao3/main.js
@@ -7,17 +7,21 @@ function carregaCSVRemoto(){
       var dadosLinhas = dados.split(/\r\n|\n/);
       var dadosLista = "";
 
-      // para cada um dos elementos
-        for (var i=1; i<100; i++) {
+      //Para cada um dos elementos
+      //Por estar começando do 1, retira-se uma unidade do tamanho da lista
+        for (var i=1; i<dadosLinhas.length-1; i++) {
             var dados = dadosLinhas[i].split(",");
             var nome = dados[4];
+            //por alguma razão o nome de algumas vem na forma de número
             var bairro = dados[1];
             var localizacao = dados[5];
             var cidade = dados[12];
             var rede = dados[13];
             var latitude = dados[6];
             var longitude = dados[7];
-            dadosLista += "<li>"+nome+", "+localizacao+", "+cidade+", "+bairro+", "+rede+", <a href='https://maps.google.com/?q="+latitude+","+longitude+"' target='_blank'>Veja no mapa</a></li>";
+            dadosLista += "<li>"+i+"- Nome do local: "+nome+", Localização: "+localizacao+", Cidade: "+cidade+", Bairro: "+bairro+", Nome da rede wi-fi: "+rede+", <a href='https://maps.google.com/?q="+latitude+","+longitude+"' target='_blank'>Veja no mapa</a></li>";
+            //Do modo como está sendo exibido, as informações as vezes não parecem condizer com o índice do cabeçalho.
+            //Não sei se é impressão minha por não conhecer as informações do lugar, ou falta de organização em algumas partes do arquivo
         }
         document.getElementById("wifi").innerHTML = dadosLista;
     });
