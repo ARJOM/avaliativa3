54628dde74250cbc94eeb2bb2a9bc2bc4559567a
Ricart <antonio.ricart@academico.ifpb.edu.br>
2019-08-12 22:44:31 -0300

    corrigindo erros de leitura de arquivo

--- questao3/main.js
+++ questao3/main.js
@@ -1,24 +1,25 @@
-function carregaCSVLocal(){
-    $.ajax({
-        type: "GET",
-        url: "wifi.csv",
-        dataType: "text",
-        success: function(data) {
-            var dadosLinhas = data.split(/\r\n|\n/);
-            var dadosLista = "";
-            
-            // para cada um dos elementos
-            for (var i=1; i<dadosLinhas.length; i++) {
-                var dados = dadosLinhas[i].split(", ");
-                var nome = dados[4];
-                var bairro = dados[1];
-                var localizacao = dados[5];
-                var cidade = dados[12];
-                var rede = dados[13];
-                var latitude = dados[6];
-                var longitude = dados[7];
-                dadosLista += "<li>"+nome+", "+localizacao+", "+cidade+", "+bairro+", "+rede+", <a href='https://maps.google.com/?q="+latitude+","+longitude+"' target='_blank'>Veja no mapa</a></li>";
-            }
-            document.getElementById("wifi").innerHTML = dadosLista;}
-        });
-}
+function carregaCSVRemoto(){
+  
+    var arquivo = 'https://raw.githubusercontent.com/ARJOM/avaliativa3/master/questao3/wifi.csv';
+  
+    $.get(arquivo, function( dados ) {
+  
+      var dadosLinhas = dados.split(/\r\n|\n/);
+      var dadosLista = "";
+
+      // para cada um dos elementos
+        for (var i=1; i<100; i++) {
+            var dados = dadosLinhas[i].split(",");
+            var nome = dados[4];
+            var bairro = dados[1];
+            var localizacao = dados[5];
+            var cidade = dados[12];
+            var rede = dados[13];
+            var latitude = dados[6];
+            var longitude = dados[7];
+            dadosLista += "<li>"+nome+", "+localizacao+", "+cidade+", "+bairro+", "+rede+", <a href='https://maps.google.com/?q="+latitude+","+longitude+"' target='_blank'>Veja no mapa</a></li>";
+        }
+        document.getElementById("wifi").innerHTML = dadosLista;
+    });
+  }
+  
